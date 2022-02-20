3cdae7505b3e15223ac6f0a889f3f140349da773
richlaverty <rich.laverty@gmail.com>
2022-02-20 15:47:41 -0500

    Initial commit with data, codebook, and first R file (plot1.R)

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# clean up the workspace
+
+rm(list = ls())
+
+# set the working directory and download the data, if necessary
+
+setwd("/home/rich/Documents/programs/data_science/04_Exploratory_Data_Analysis/Exploratory_Data_Project2")
+
+# import the data
+
+if(file.exists("./data/summarySCC_PM25.rds"))
+{
+  NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(file.exists("./data/Source_Classification_Code.rds"))
+{
+  SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+

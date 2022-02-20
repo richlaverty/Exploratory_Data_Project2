3b9a3994ef32f406a103e921045ce8a5ddf999aa
richlaverty <rich.laverty@gmail.com>
2022-02-20 16:22:12 -0500

    Made plot1 which contains the total PM2.5 emissions for each year from all soures.

--- plot1.R
+++ plot1.R
@@ -1,3 +1,11 @@
+# Question to answer:
+# Have total emissions from PM2.5 decreased in the United States
+# from 1999 to 2008? 
+
+# Using the base plotting system, make a plot showing the total 
+# PM2.5 emission from all sources for each of the years 1999, 2002, 
+# 2005, and 2008.
+
 # clean up the workspace
 
 rm(list = ls())
@@ -16,4 +24,18 @@ if(file.exists("./data/Source_Classification_Code.rds"))
 {
   SCC <- readRDS("./data/Source_Classification_Code.rds")
 }
+
+# get the total emissions for each year
+
+totalEmissions <- tapply(NEI$Emissions, NEI$year, sum)
+
+# make the plot
+
+while(dev.cur() > 1) { dev.off() }
+x11()
+plot(as.numeric(names(totalEmissions)), totalEmissions, pch = 19, 
+     main = "Sum of PM2.5 Emissions from all Sources",
+     xlab = "Year", ylab = "PM2.5 Emissions (Tons)")
+dev.copy(png, file = "plot1.png", height = 480, width = 480, units = "px")
+dev.off() # writes the file, but leaves the screen device open
 

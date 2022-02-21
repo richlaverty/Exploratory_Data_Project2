fc3c90b02bb85c4c4d4f2e0326d08d292dbacfc2
richlaverty <rich.laverty@gmail.com>
2022-02-21 18:20:23 -0500

    plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -28,3 +28,33 @@ baltPointNEI <- subset(NEI, NEI$fips == "24510" & NEI$type == "POINT")
 baltNonPointNEI <- subset(NEI, NEI$fips == "24510" & NEI$type == "NONPOINT")
 baltOnRoadNEI <- subset(NEI, NEI$fips == "24510" & NEI$type == "ON-ROAD")
 baltNonRoadNEI <- subset(NEI, NEI$fips == "24510" & NEI$type == "NON-ROAD")
+
+# get the total emissions, by year, for each of the 4 data sets
+pointTotal <- tapply(baltPointNEI$Emissions, baltPointNEI$year, sum)
+nonPointTotal <- tapply(baltNonPointNEI$Emissions, baltNonPointNEI$year, sum)
+onRoadTotal <- tapply(baltOnRoadNEI$Emissions, baltOnRoadNEI$year, sum)
+nonRoadTotal <- tapply(baltNonRoadNEI$Emissions, baltNonRoadNEI$year, sum)
+
+# create one data frame with all the total emissions data
+totalEmissions <- data.frame(Year = rep(as.numeric(names(pointTotal)),4))
+totalEmissions$Emissions <- c(pointTotal, nonPointTotal, onRoadTotal, nonRoadTotal)
+totalEmissions$type <- as.factor(rep(c("POINT", "NONPOINT", "ON-ROAD", "NON-ROAD"), each = 4))
+
+# create the plot
+
+library(ggplot2)
+while(dev.cur() > 1) { dev.off() }
+g <- ggplot(totalEmissions, aes(Year, Emissions))
+g <- g + geom_point()
+g <- g + geom_smooth(method = lm, se = FALSE)
+g <- g + facet_grid(. ~ type)
+g <- g + labs("Total PM2.5 Emissions in Baltimore by Type")
+g <- g + labs(x = "Year", y = "Total Emissions (Tons)")
+x11()
+print(g)
+dev.copy(png, file = "plot3.png", height = 480, width = 720, units = "px")
+dev.off() # writes the file, but leaves the screen device open
+
+
+
+

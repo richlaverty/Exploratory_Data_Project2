80dfad88440f888862bc8f2c3eefd269a2086eaa
richlaverty <rich.laverty@gmail.com>
2022-03-06 19:14:00 -0500

    plot4.R with plot4.png - it looks like a decrease in PM25 for coal combustion

--- plot4.R
+++ plot4.R
@@ -20,7 +20,24 @@ if(file.exists("./data/Source_Classification_Code.rds"))
   SCC <- readRDS("./data/Source_Classification_Code.rds")
 }
 
-# get the relevant data
+# get the relevant data rows
 combustion <- SCC[grep("[Cc]ombust", SCC$SCC.Level.One), ]
-coalCombustion <- combutions[grep("[Cc]oal", SCC$combustion$SCC.Level.Three), ]
+coalCombustion <- combustion[grep("[Cc]oal", combustion$SCC.Level.Three), ]
+SCCCodes <- unique(coalCombustion$SCC)
+
+# get the total emissions for each year
+
+coalCombNEI <- filter(NEI, SCC %in% SCCCodes)
+totalEmissions <- tapply(coalCombNEI$Emissions, coalCombNEI$year, sum)
+
+# make the plot
+
+while(dev.cur() > 1) { dev.off() }
+x11()
+plot(as.numeric(names(totalEmissions)), totalEmissions, pch = 19, 
+     main = "Sum of PM2.5 Emissions from Coal Combustion",
+     xlab = "Year", ylab = "PM2.5 Emissions (Tons)")
+abline(lm(totalEmissions ~ as.numeric(names(totalEmissions))), lwd = 3, col = "blue")
+dev.copy(png, file = "plot4.png", height = 480, width = 480, units = "px")
+dev.off() # writes the file, but leaves the screen device open
 
